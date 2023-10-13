91257c5df4feea1570070a2183022d98e3747806
Albert Hoekman <Hoekman.albert@hotmail.com>
2023-10-13 16:36:34 +0200

    moi

--- shell2.sh
+++ shell2.sh
@@ -5,10 +5,9 @@
 #SBATCH --ntasks=1
 #SBATCH --mem=64GB
 #SBATCH --partition=regular
-#SBATCH --array=0-10
 
 module purge
 module load OpenBLAS/0.3.18-GCC-11.2.0
 
 source /home4/s3537307/venv/bin/activate
-pypy /home4/s3537307/Scriptie2/main2.py ${SLURM_ARRAY_TASK_ID}
+pypy /home4/s3537307/Scriptie2/main2.py 0
